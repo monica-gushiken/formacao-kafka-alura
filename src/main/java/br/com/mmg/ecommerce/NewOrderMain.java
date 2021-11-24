931f4fdb023af78e3da6dc14af4349756fed0191
Monica Mayumi Gushiken <monica-gushiken@users.noreply.github.com>
2021-11-23 21:34:18 -0300

    Incluindo novos consumidores e um consumidor de logs que assina diversos tópicos

--- src/main/java/br/com/mmg/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/mmg/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
-package  br.com.mmg.ecommerce;
+package br.com.mmg.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -12,19 +13,23 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "123123,65734,1234";
-        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value,value);
-        producer.send(record, (data, ex)->{
-            if (ex != null){
+        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("sucesso enviando " + data.topic()+ "::: partition " + data.partition() + "/ offset " + data.offset()+"/ timestamp " + data.timestamp());
-        }).get();
+            System.out.println("sucesso enviando " + data.topic() + "::: partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
+        };
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
         var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return properties;
